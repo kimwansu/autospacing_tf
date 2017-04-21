81552f47e9297b4bd1e01c7494fdd58d17cba229
Kim Wansu <wantalia@gmail.com>
2017-04-21 14:15:32 +0900

    add interference function.

--- bi_lstm.py
+++ bi_lstm.py
@@ -5,12 +5,12 @@ from tflearn.data_utils import to_categorical, pad_sequences
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.embedding_ops import embedding
 from tflearn.layers.recurrent import bidirectional_rnn, BasicLSTMCell
-from tflearn.layers.recurrent import lstm
 from tflearn.layers.estimator import regression
 from tflearn.optimizers import *
 
 from multiprocessing import cpu_count, freeze_support
 from multiprocessing.pool import Pool
+import sys
 
 from make_data import make_data
 from make_data import norm_many
@@ -49,8 +49,16 @@ def train(trainX, trainY, model_file):
     2: Loss, Accuracy, Gradients, Weights
     3: Loss, Accuracy, Gradients, Weights, Activations, Sparsity (Best Visualization)
     '''
-    model = tflearn.DNN(net, clip_gradients=0., tensorboard_verbose=0)
-    model.fit(trainX, trainY, validation_set=0.1, show_metric=True, batch_size=128)
+    model = tflearn.DNN(net, clip_gradients=0., tensorboard_verbose=0,
+                        checkpoint_path='./chkpoint/',
+                        best_checkpoint_path='./best_chkpoint/',
+                        best_val_accuracy=0.9)
+
+    # show_metric: If True, accuracy will be calculated and displayed
+    #              at every step. Might give slower training.
+    model.fit(trainX, trainY, validation_set=0.1, show_metric=False,
+              batch_size=128, n_epoch=1,
+              run_id='bLSTM_i440_20k_d9_o128_d7_adam_l001_b128')
 
     # Save model
     model.save(model_file)
@@ -64,20 +72,44 @@ def interference(testX, testY, model_file):
     net = bi_LSTM()
 
     # Load model
-    model = tflearn.DNN(net, clip_gradients=0., tensorboard_verbose=2)
+    model = tflearn.DNN(net, clip_gradients=0., tensorboard_verbose=0)
     model.load(model_file)
 
     # Predict
     pred = model.predict(testX)
+    return pred
 
-def main():
+def test_train():
     pool = Pool(processes=cpu_count())
     X, Y = make_data(pool, 'ted_7_ErasePunc_FullKorean__train.txt')
-    print('make data end.')
+    print('make train data end.')
     X = norm_many(pool, X)
-    print('norm_data end.')
+    print('norm train data end.')
+
     train(X, Y, 'model.tfl')
 
+def test_interference():
+    pool = Pool(processes=cpu_count())
+    X, Y = make_data(pool, 'ted_7_ErasePunc_FullKorean__test.txt')
+    print('make test data end.')
+    X = norm_many(pool, X)
+    print('norm test data end.')
+
+    pred = interference(X, Y, 'model.tfl')
+    print('pred[:10]={}'.format(pred[:10]))
+
+def usage():
+    print('Usage: python bi_lstm.py (train|interference)')
+
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        usage()
+        sys.exit(1)
+
     freeze_support()
-    main()
+    if sys.argv[1] == 'train':
+        test_train()
+    elif sys.argv[1] == 'interference':
+        test_interference()
+    else:
+        usage()
